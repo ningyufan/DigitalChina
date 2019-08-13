55eff0c87f34cca23f49e1e0b46778aef8bc5c95
ningyufan <223_ningyufan@computer162.com>
2019-08-13 18:54:05 +0800

    nyf

--- components/GX_card.vue
+++ components/GX_card.vue
@@ -30,6 +30,55 @@ export default {
 
 <style>
     @import "@/assets/css/DigitalGX.css";
+.GX_card {
+    width: 88%;
+    height: 100%;
+    margin: 3vh 5vw auto;
+}
 
+.one_row, .three_row {
+    height: 47%;
+    width: 100%;
+ 
+}
+.one_col,.two_col {
+    height: 100%;
+    width: 50%;
+    
+}
+.one_col {
+    background-image: url(../assets/images//kuang1.png);
+    background-size: 100% 100%;
+}
+.two_col {
+    background-image: url(../assets/images//kuang2.png);
+    background-size: 100% 100%;
+}
+.two_row {
+    width: 100%;
+    height: 6%;
+    margin: 0 auto;
+    padding-left: 47%;
+}
+.t_one_col, .t_two_col, .t_three_col,.t_four_col{
+    height: 100%;
+    width: 25%;
+}
+.t_one_col{
+    background-image: url(../assets/images//kuang3.png);
+    background-size: 100% 100%;
+}
+.t_two_col {
+    background-image: url(../assets/images//kuang4.png);
+    background-size: 100% 100%;
+}
+.t_three_col {
+    background-image: url(../assets/images//kuang5.png);
+    background-size: 100% 100%;
+}
+.t_four_col {
+    background-image: url(../assets/images//kuang6.png);
+    background-size: 100% 100%;
+}
 </style>
     
