f6cfe458f4216c5bf054895c0384af36e676191a
lushimei <lushimei@email.com>
2019-08-13 10:09:57 +0800

    dsf

---
+++ components/dchina/mk.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="china_mk">
+        <div class="mk_header">
+        <el-row class="china_row">
+            <el-col :span="8" class="china_col">
+                <div class="china_mk1">
+                    <!-- <img src="@/assets/images/dchina/dbg.png" alt=""> -->
+                </div>
+            </el-col>
+            <el-col :span="8" class="china_col">
+                <div class="china_mk2"></div>
+            </el-col>
+        </el-row>
+        </div>
+        <div class="mk_center">
+        <el-row class="fangkuai">
+            <!-- <img src="@/assets/images/dchina/cfk.png" alt=""> -->
+        </el-row>
+        </div>
+        <div class="mk_footer">
+        <el-row class="china_row">
+            <el-col :span="12" class="china_col">
+                <div class="china_mk3"></div>
+                <div class="china_mk4"></div>
+            </el-col>
+            <el-col :span="12" class="china_col">
+                <div class="china_mk5"></div>
+                <div class="china_mk6"></div>
+            </el-col>
+        </el-row>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style>
+    @import "@/assets/css/digital_china.css";
+</style>
