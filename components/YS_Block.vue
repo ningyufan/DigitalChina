5f583ce3c3104772ed3a1f0a853c6bdf3eeff6e7
Your Name <email@example.com>
2019-08-15 21:07:44 +0800

    xx

--- components/YS_Block.vue
+++ components/YS_Block.vue
@@ -4,24 +4,29 @@
             <div class="one">
                 <div class="box1">
                     <div class="ys1">
-                        <img src="../assets/images/ysblock/ys1.png" class="img1" height="100%" width="100%">
-                        <div class="text1">玉林师范学院——曙光大数据应用创新中心揭牌成立</div>
+                        <a href="#"><img src="../assets/images/ysblock/ys1.png" class="img1" height="100%" width="100%"></a>
+                        <a href="#"><div class="text1">玉林师范学院——曙光大数据应用创新中心揭牌成立</div></a>
+                        <div class="line1"></div>
                     </div>
                     <div class="ys2">
-                        <img src="../assets/images/ysblock/ys2.png" class="img2" height="100%" width="100%">
-                        <div class="text2">计算机学院积极推进“玉林师范学院——曙光大数据<br>学院”的建设</div>
+                        <a href="#"><img src="../assets/images/ysblock/ys2.png" class="img2" height="100%" width="100%"></a>
+                        <a href="#"><div class="text2">计算机学院积极推进“玉林师范学院——曙光大数据学院”的建设</div></a>
+                        <div class="line2"></div>
                     </div>
                     <div class="ys3">
-                        <img src="../assets/images/ysblock/ys3.png" class="img3" height="100%" width="100%">
-                        <div class="text3">学校举行市校共建玉林市大数据研究院揭牌仪式暨签<br>约仪式</div>
+                        <a href="#"><img src="../assets/images/ysblock/ys3.png" class="img3" height="100%" width="100%"></a>
+                        <a href="#"><div class="text3">学校举行市校共建玉林市大数据研究院揭牌仪式暨签约仪式</div></a>
+                        <div class="line3"></div>
                     </div>
                     <div class="ys4">
-                        <img src="../assets/images/ysblock/ys4.png" class="img4" height="100%" width="100%">
-                        <div class="text4">曙光集团莅临大数据学院开展曙光大数据平台使用培<br>训会</div>
+                        <a href="#"><img src="../assets/images/ysblock/ys4.png" class="img4" height="100%" width="100%"></a>
+                        <a href="#"><div class="text4">曙光集团莅临大数据学院开展曙光大数据平台使用培训会</div></a>
+                        <div class="line4"></div>
                     </div>
                     <div class="ys5">
-                        <img src="../assets/images/ysblock/ys5.png" class="img5" height="100%" width="100%">
-                        <div class="text5">学校领导到大数据应用创新中心（暨大数据学院）指<br>导工作</div>
+                        <a href="#"><img src="../assets/images/ysblock/ys5.png" class="img5" height="100%" width="100%"></a>
+                        <a href="#"><div class="text5">学校领导到大数据应用创新中心（暨大数据学院）指导工作</div></a>
+                        <div class="line5"></div>
                     </div>
                 </div>
             </div>
@@ -49,7 +54,6 @@ export default {
 .one{
     width: 70%;
     height: 100%;
-
 }
 .tow{
     width: 30%;
@@ -66,7 +70,6 @@ export default {
     text-align: center;
     margin-top: 3.8vh;
     margin-left: 15%;
-
 }
 .ys1{
     height: 60%;
@@ -81,8 +84,17 @@ export default {
 .text1{
     position: absolute;
     color: white;
-    margin-left: 45%;
+    margin-left: 48.8%;
     float: left;
+    margin-top: 3vh;
+    height: 8%;
+    width: 22%;
+}
+.line1{
+    background-image: url(../assets/images/ysblock/line1.png);
+    height: 23.5%;
+    width: 110%;
+    margin-left: 175%;
     margin-top: 3vh;
 }
 .ys2{
@@ -98,9 +110,18 @@ export default {
 .text2{
     position: absolute;
     color: white;
-    margin-left: 19.8%;
+    margin-left: 23.8%;
     float: left;
-    margin-top: 9vh;
+    margin-top: 12vh;
+    height: 8%;
+    width: 22%;
+}
+.line2{
+    background-image: url(../assets/images/ysblock/line1.png);
+    height: 48%;
+    width: 185%;
+    margin-left: 123%;
+    margin-top: 12vh;
 }
 .ys3{
     height: 29.5%;
@@ -117,9 +138,18 @@ export default {
 .text3{
     position: absolute;
     color: white;
-    margin-left: 19.8%;
+    margin-left: 23.5%;
     float: left;
-    margin-top: 3vh;
+    margin-top: 6vh;
+    height: 8%;
+    width: 22%;
+}
+.line3{
+    background-image: url(../assets/images/ysblock/line1.png);
+    height: 48%;
+    width: 185%;
+    margin-left: 123%;
+    margin-top: 6vh;
 }
 .ys4{
     height: 39%;
@@ -135,9 +165,18 @@ export default {
 .text4{
     position: absolute;
     color: white;
-    margin-left: 45%;
+    margin-left: 48.8%;
     float: left;
-    margin-top: -3vh;
+    margin-top: 0vh;
+    height: 8%;
+    width: 22%;
+}
+.line4{
+    background-image: url(../assets/images/ysblock/line1.png);
+    height: 36.5%;
+    width: 138%;
+    margin-left: 219%;
+    margin-top: 0vh;
 }
 .ys5{
     height: 39%;
@@ -153,9 +192,103 @@ export default {
 .text5{
     position: absolute;
     color: white;
-    margin-left: 25%;
+    margin-left: 28.5%;
     float: left;
-    margin-top: 6vh;
+    margin-top: 9vh;
+    height: 8%;
+    width: 22%;
+}
+.line5{
+    background-image: url(../assets/images/ysblock/line1.png);
+    height: 36.5%;
+    width: 138%;
+    margin-left: 117%;
+    margin-top: 9vh;
+}
+img{
+    -webkit-transform:scale(1);
+	-moz-transform:scale(1);
+	-o-transform:scale(1);
+    -webkit-transition-duration: 0.5s;
+	-moz-transition-duration: 0.5s;
+    -o-transition-duration: 0.5s;
+    z-index: 2;
+    position: relative;
+}
+img:hover{
+    border: 2px solid white;
+    -webkit-transform:scale(1.2);
+	-webkit-box-shadow:0px 0px 30px #ccc;
+	-moz-transform:scale(1.2);
+	-moz-box-shadow:0px 0px 30px #ccc;	
+	-o-transform:scale(1.2);
+	-o-box-shadow:0px 0px 30px #ccc;
+    z-index: 3;
+}
+.ys1:hover .text1{
+    color: blue;
+}
+.ys1:hover .img1{
+    border: 2px solid white;
+    -webkit-transform:scale(1.2);
+	-webkit-box-shadow:0px 0px 30px #ccc;
+	-moz-transform:scale(1.2);
+	-moz-box-shadow:0px 0px 30px #ccc;	
+	-o-transform:scale(1.2);
+	-o-box-shadow:0px 0px 30px #ccc;
+    z-index: 3;
+}
+.ys2:hover .text2{
+    color: blue;
+}
+.ys2:hover .img2{
+    border: 2px solid white;
+    -webkit-transform:scale(1.2);
+	-webkit-box-shadow:0px 0px 30px #ccc;
+	-moz-transform:scale(1.2);
+	-moz-box-shadow:0px 0px 30px #ccc;	
+	-o-transform:scale(1.2);
+	-o-box-shadow:0px 0px 30px #ccc;
+    z-index: 3;
+}
+.ys3:hover .text3{
+    color: blue;
+}
+.ys3:hover .img3{
+    border: 2px solid white;
+    -webkit-transform:scale(1.2);
+	-webkit-box-shadow:0px 0px 30px #ccc;
+	-moz-transform:scale(1.2);
+	-moz-box-shadow:0px 0px 30px #ccc;	
+	-o-transform:scale(1.2);
+	-o-box-shadow:0px 0px 30px #ccc;
+    z-index: 3;
+}
+.ys4:hover .text4{
+    color: blue;
+}
+.ys4:hover .img4{
+    border: 2px solid white;
+    -webkit-transform:scale(1.3);
+	-webkit-box-shadow:0px 0px 30px #ccc;
+	-moz-transform:scale(1.3);
+	-moz-box-shadow:0px 0px 30px #ccc;	
+	-o-transform:scale(1.3);
+	-o-box-shadow:0px 0px 30px #ccc;
+    z-index: 3;
+}
+.ys5:hover .text5{
+    color: blue;
+}
+.ys5:hover .img5{
+    border: 2px solid white;
+    -webkit-transform:scale(1.2);
+	-webkit-box-shadow:0px 0px 30px #ccc;
+	-moz-transform:scale(1.2);
+	-moz-box-shadow:0px 0px 30px #ccc;	
+	-o-transform:scale(1.2);
+	-o-box-shadow:0px 0px 30px #ccc;
+    z-index: 3;
 }
 
 </style>
