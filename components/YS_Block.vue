fb6ab29240354849e48ec28d0e32725589f40fa2
Your Name <email@example.com>
2019-08-15 14:21:32 +0800

    dd

--- components/YS_Block.vue
+++ components/YS_Block.vue
@@ -1,6 +1,32 @@
 <template>
-    <div>
-        数字玉师
+    <div class="ysblock">
+        <div class="row">    
+            <div class="one">
+                <div class="box1">
+                    <div class="ys1">
+                        <img src="../assets/images/ysblock/ys1.png" class="img1" height="100%" width="100%">
+                        <div class="text1">玉林师范学院——曙光大数据应用创新中心揭牌成立</div>
+                    </div>
+                    <div class="ys2">
+                        <img src="../assets/images/ysblock/ys2.png" class="img2" height="100%" width="100%">
+                        <div class="text2">计算机学院积极推进“玉林师范学院——曙光大数据<br>学院”的建设</div>
+                    </div>
+                    <div class="ys3">
+                        <img src="../assets/images/ysblock/ys3.png" class="img3" height="100%" width="100%">
+                        <div class="text3">学校举行市校共建玉林市大数据研究院揭牌仪式暨签<br>约仪式</div>
+                    </div>
+                    <div class="ys4">
+                        <img src="../assets/images/ysblock/ys4.png" class="img4" height="100%" width="100%">
+                        <div class="text4">曙光集团莅临大数据学院开展曙光大数据平台使用培<br>训会</div>
+                    </div>
+                    <div class="ys5">
+                        <img src="../assets/images/ysblock/ys5.png" class="img5" height="100%" width="100%">
+                        <div class="text5">学校领导到大数据应用创新中心（暨大数据学院）指<br>导工作</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </template>
 
@@ -11,5 +37,125 @@ export default {
 </script>
 
 <style>
+.ysblock{
+    width: 100%;
+    height: 100%;
+    margin: 5vh -5vh auto;
+}
+.row{
+    height: 100%;
+    width:100%;
+}
+.one{
+    width: 70%;
+    height: 100%;
+
+}
+.tow{
+    width: 30%;
+    height: 100%;
+}
+.one{
+    background-image: url(../assets/images/ysblock/block.png);
+    background-size: 100% 100%;
+    border: 2px solid transparent;
+}
+.box1{
+    height: 82%;
+    width: 72%;
+    text-align: center;
+    margin-top: 3.8vh;
+    margin-left: 15%;
+
+}
+.ys1{
+    height: 60%;
+    width: 62%;
+    float: left;
+}
+.img1{
+    height: 100%;
+    width: 100%;
+    float: left;
+}
+.text1{
+    position: absolute;
+    color: white;
+    margin-left: 45%;
+    float: left;
+    margin-top: 3vh;
+}
+.ys2{
+    height: 29.5%;
+    width: 37%;
+    float: right;
+}
+.img2{
+    height: 100%;
+    width: 100%;
+    float: right;
+}
+.text2{
+    position: absolute;
+    color: white;
+    margin-left: 19.8%;
+    float: left;
+    margin-top: 9vh;
+}
+.ys3{
+    height: 29.5%;
+    width: 37%;
+    float: right;
+    margin-top:0.8%;
+}
+.img3{
+    height: 100%;
+    width: 100%;
+    float: right;
+
+}
+.text3{
+    position: absolute;
+    color: white;
+    margin-left: 19.8%;
+    float: left;
+    margin-top: 3vh;
+}
+.ys4{
+    height: 39%;
+    width: 49.5%;
+    float: left;
+    margin-top: 1%;
+}
+.img4{
+    height: 100%;
+    width: 100%;
+    float: left;
+}
+.text4{
+    position: absolute;
+    color: white;
+    margin-left: 45%;
+    float: left;
+    margin-top: -3vh;
+}
+.ys5{
+    height: 39%;
+    width: 49.5%;
+    float: right;
+    margin-top: 1%;
+}
+.img5{
+    height: 100%;
+    width: 100%;
+    float: right;
+}
+.text5{
+    position: absolute;
+    color: white;
+    margin-left: 25%;
+    float: left;
+    margin-top: 6vh;
+}
 
 </style>
