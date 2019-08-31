9100c37ae010463bbe702fa9f139eafcb6e228d2
lushimei <lushimei@email.com>
2019-08-31 10:34:55 +0800

    lsm19.08.31

--- pages/en_ct2.vue
+++ pages/en_ct2.vue
@@ -1,79 +1,79 @@
 <template>
     <div class="cn_ct2">
         <div class="cn_bt">
-            25省区市公布上半年成绩单:新兴动能贡献突出
+            25 provinces, districts and municipalities announce the first half of the report card: the contribution of emerging momentum is outstanding
         </div>
-        <img src="../assets/images/dchina/szjj2.jpg" alt="" height="50%" width="80%">
+        <img src="../assets/images/dchina/szjj2.jpg" alt="" height="50%" width="60%">
         <div class="cn_info">
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                随着各省区市今年上半年经济运行“成绩单”陆续出炉，各地经济显示出平稳运行势头，经济增速处于合理区间。不过，面对错综复杂的国际国内形势，下一阶段，各地还要进一步深化改革，优化营商环境，激发市场主体活力，争取在下半年经济运行中交出更好的答卷——
+                With the "report card" of economic operation of provinces, districts and municipalities coming out in the first half of this year, the local economy shows a steady momentum of operation, and the economic growth rate is in a reasonable range. However, in the face of the complex international and domestic situation, in the next stage, all localities should further deepen reform, optimize business environment, stimulate the vitality of market players, and strive for better answers in the second half of the economic operation.——
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                近期，多个省区市陆续公布了2019年上半年经济运行“成绩单”。截至7月29日，共有25个省区市公布了主要经济指标数据。其中，云南、贵州、江西等14个省区市经济增速快于全国6.3%的GDP增速；北京的增速与全国持平；重庆、甘肃等10个省区市的增速慢于全国增速。
+                Recently, many provinces, autonomous regions and municipalities have published the "report card" of economic operation in the first half of 2019. As of July 29, 25 provinces, autonomous regions and municipalities have published data on major economic indicators. Among them, 14 provinces, such as Yunnan, Guizhou and Jiangxi, have a faster economic growth rate than 6.3% of GDP in the whole country; Beijing's growth rate is the same as that in the whole country; and 10 provinces, such as Chongqing and Gansu, have a slower growth rate than that in the whole country.
             </p><br/>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;经济增速处于合理区间</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Economic growth is in a reasonable range</p>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                在东部地区的10个省份中，福建实现地区生产总值16640.15亿元，同比增长8.1%，在东部地区处于领跑地位。浙江紧随其后，上半年GDP增速为7.1%，比一季度放缓0.6个百分点，增速高于全国0.8个百分点，且明显高于年初确定的6.5%左右的目标增速。北京的GDP增速为6.3%，与全国经济增速持平，符合年初确定的6.0%至6.5%的增速预期。
-            </p><br/>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                天津上半年GDP增长4.6%，在已经公布经济数据的25个省区市中垫底。天津市统计局有关负责人表示，近两年天津在向高质量发展转变的过程中，经济进入爬坡过坎、负重前行的战略调整阶段，发展遇到了一些困难，但天津经济发展的基础还在，基本面没有变。
+                Among the 10 provinces in the eastern region, Fujian has achieved 1664.015 billion yuan of GDP, an increase of 8.1% over the same period last year, leading the way in the eastern region. In the first half of the year, Zhejiang's GDP growth rate was 7.1%, 0.6 percentage points slower than that of the first quarter, 0.8 percentage points higher than that of the whole country, and significantly higher than the target growth rate of about 6.5% set at the beginning of the year. Beijing's GDP growth rate is 6.3%, which is the same as the national economic growth rate, which is in line with the 6.0% to 6.5% growth forecast set at the beginning of the year.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-               中部6省份上半年的经济增速均跑赢全国GDP增速，且除湖南外，其余5个省份的增速跑赢目标增速或位于目标增速上限。其中，江西以8.6%的增速在中部地区领跑，这一增速快于8.0%至8.5%的目标增速上限；安徽、湖北的增速均为8.0%，这一增速均处于7.5%至8.0%的目标增速上限；河南的增速为7.7%，快于7.0%至7.5%的目标增速上限；山西的增速为7.2%，快于6.3%左右的目标增速。
+                Tianjin's GDP grew by 4.6% in the first half of the year, bottoming out in 25 provinces, autonomous regions and municipalities that have published economic data. The responsible person of Tianjin Statistical Bureau said that in the process of Tianjin's transformation to high-quality development in the past two years, the economy has entered the strategic adjustment stage of climbing the slope and crossing the ridge and carrying heavy loads. The development has encountered some difficulties, but the foundation of Tianjin's economic development is still there and the fundamentals remain unchanged.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                在西部地区，云南的经济增速为9.2%，虽然比一季度9.7%的增速放缓0.5个百分点，但在西部已公布成绩单的省区市中处于领跑位置；贵州紧随其后，经济增速为9.0%，虽然比一季度放缓0.2个百分点，仍符合9.0%左右的预期目标。
+               In the first half of the year, the economic growth of the six provinces in central China outperformed the GDP growth rate of the whole country. Besides Hunan, the other five provinces outperformed the target growth rate or were at the upper limit of the target growth rate. Among them, Jiangxi takes the lead in the central region with an 8.6% growth rate, which is faster than the target growth limit of 8.0% to 8.5%; Anhui and Hubei both have an 8.0% growth rate, which is in the target growth limit of 7.5% to 8.0%; Henan's growth rate is 7.7%, faster than the target growth limit of 7.0% to 7.5%; Shanxi's growth rate is 7.2%, faster than the target growth limit of 6.0%. The target growth rate is about 3%.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                甘肃的经济增速为6.0%，比一季度放缓1.9个百分点，但比去年同期加快了1.0个百分点。甘肃省统计局巡视员、新闻发言人刘光华表示，甘肃将抓实抓细“大的不行小的补、重的不行轻的补、二产乏力三产补、堤内损失堤外补”措施，着力破难点、解痛点、疏堵点。
+                In the western region, Yunnan's economic growth rate was 9.2%. Although it slowed down by 0.5 percentage points from 9.7% in the first quarter, it was in the leading position in the provinces, districts and municipalities in the western region that had published the report card. Guizhou followed suit, its economic growth rate was 9.0%. Although it slowed down by 0.2 percentage points from the first quarter, it still met the expected target of about 9.0%.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                东北三省，黑龙江和吉林的经济增速尚未公布，辽宁上半年的增速为5.8%，比一季度放缓0.3个百分点。从相关指标看，金融信贷增长较好，货运量保持增长，用电量增长平稳。
+                Gansu's economic growth rate was 6.0%, 1.9 percentage points slower than the first quarter, but 1.0 percentage points faster than the same period last year. Liu Guanghua, an inspector and spokesman of Gansu Statistical Bureau, said that Gansu would focus on the measures of "big not small, heavy not light, second, weak, third, loss of dike outside" to break through difficulties, relieve pain and dredge blockages.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-               总的来看，上半年，尽管有16个省区市的GDP增速较一季度有所放缓，但有17个省区市的经济增速达到甚至好于年初确定的预期增长目标，为完成全年增长目标任务打下了较好基础。
+                In the three northeastern provinces, Heilongjiang and Jilin's economic growth rate has not yet been announced, and Liaoning's growth rate in the first half of the year was 5.8%, slowing down by 0.3 percentage points from the first quarter. From the relevant indicators, the growth of financial credit is good, freight volume keeps growing, and electricity consumption grows steadily.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                逆周期调节效果显现
+               Generally speaking, in the first half of the year, although the GDP growth rate of 16 provinces and municipalities slowed down compared with the first quarter, the economic growth rate of 17 provinces and municipalities reached or even better than the expected growth target set at the beginning of the year, which laid a good foundation for fulfilling the growth target task for the whole year.
             </p><br/>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;国家统计局新闻发言人毛盛勇表示，今年上半年，为了应对国内外压力，中央出台了一系列政策，包括积极的财政政策加力提效，稳健的货币政策灵活适度，就业优先的政策全面发力等。
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Reverse Cycle Regulation Effect Appearance
+            </p><br/>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mao Shengyong, spokesman for the National Bureau of Statistics, said that in the first half of this year, in response to domestic and international pressures, the central government issued a series of policies, including a positive fiscal policy to enhance effectiveness, a sound monetary policy to be flexible and moderate, and a comprehensive employment priority policy.
             </p><br>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                国家统计局新闻发言人毛盛勇表示，今年上半年，为了应对国内外压力，中央出台了一系列政策，包括积极的财政政策加力提效，稳健的货币政策灵活适度，就业优先的政策全面发力等。
+                Mao Shengyong, spokesman for the National Bureau of Statistics, said that in the first half of this year, in response to domestic and international pressures, the central government issued a series of policies, including a positive fiscal policy to enhance effectiveness, a sound monetary policy to be flexible and moderate, and a comprehensive employment priority policy.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                各地区各部门认真贯彻落实中央决策部署，以市场化改革的思路和办法破解发展难题，发挥好宏观政策逆周期调节作用，为经济平稳运行创造条件。
+                All departments in various regions have earnestly implemented the central decision-making plan, solved the development problems with the ideas and methods of market-oriented reform, played a good role in the counter-cyclical adjustment of macro-policies, and created conditions for the smooth operation of the economy.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-               北京积极推动减量集约发展，上半年，全市规模以上法人单位减少195家。同时，深入推进改革开放，全面贯彻落实减税降费政策，优化发展环境。
+               Beijing actively promotes the development of reduction and intensive. In the first half of the year, the number of corporate units above the city's scale decreased by 195. At the same time, we will further promote reform and opening up, comprehensively implement the policy of tax reduction and fee reduction, and optimize the development environment.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                天津不搞“大水漫灌”式的强刺激，下大力气推进改革创新，突出智能和科技的产业政策引领，同时继续压缩高耗能产业，落实好国家各项减税降费举措，市场信心不断增强。
+                Tianjin does not engage in "flood irrigation" type of strong stimulus, vigorously promote reform and innovation, highlight the industrial policy of intelligence and technology, while continuing to compress energy-intensive industries, implement various national tax reduction and fee reduction measures, and increase market confidence.
             </p><br/>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;福建“放管服”改革持续深化，减税降费成效显现，市场活跃度进一步增强。上半年，该省新登记市场主体106.3万户，增长50.7%。</p><br/>
-        　　<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;江苏推进供给侧结构性改革，实施大规模减税降费，助推企业效益水平提升。前5个月，该省规模以上工业企业营业收入利润率为5.4%，比一季度提高0.5个百分点。</p><br/>
-        　　<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;“下一阶段，还要进一步深化改革，优化营商环境，进一步激发市场主体的活力，争取下半年经济运行有更好的表现。”毛盛勇说。</p><br/>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;新经济贡献率明显提升</p><br>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fujian's reform of "releasing control clothing" continued to deepen, the effect of tax reduction and fee reduction was obvious, and the market activity was further enhanced. In the first half of the year, the main body of the new registration market in the province was 1.063 million households, an increase of 50.7%.</p><br/>
+        　　<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jiangsu has promoted structural reform on the supply side, implemented large-scale tax reduction and fee reduction, and promoted the improvement of enterprise efficiency. In the first five months, the profit margin of operating income of Industrial Enterprises above the scale of the province was 5.4%, which was 0.5 percentage points higher than that of the first quarter.</p><br/>
+        　　<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"In the next stage, we should further deepen the reform, optimize the business environment, further stimulate the vitality of market players, and strive for better economic performance in the second half of the year." Mao Shengyong said.</p><br/>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The contribution rate of the new economy has increased significantly</p><br>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                随着各项改革措施的落实落地，新产业、新产品、新业态、新模式不断成长，新经济在地区经济发展中扮演着越来越突出的作用。
-                今年上半年，北京新经济实现增加值占全市经济比重保持在三分之一左右，其中，金融、科技、信息等优势行业对经济增长的贡献率合计接近七成。
-            </p><br/>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                天津出台《天津市促进数字经济发展行动方案(2019-2023年)》，以数字经济为载体推动新业态的蓬勃发展，上半年全市限额以上单位网上零售额稳步增长。
+               With the implementation of various reform measures, new industries, new products, new formats and new models continue to grow, and the new economy plays an increasingly prominent role in regional economic development.
+                In the first half of this year, the added value of Beijing's new economy accounted for about one third of the city's economy. Among them, the contribution rate of financial, science and technology, information and other dominant industries to economic growth totaled nearly 70%.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-               浙江聚力动能转换，推动数字经济相关产业保持较快增长。上半年，数字经济核心产业增加值同比增长14.8%。规模以上工业新产品产值增长13.7%，新产品产值率为35.7%。
+                Tianjin promulgated "Tianjin Action Plan for Promoting Digital Economy Development (2019-2023)", which takes digital economy as the carrier to promote the vigorous development of the new format. In the first half of the year, the online retail sales of units above the quota in Tianjin increased steadily.
             </p><br/>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                山西工业新动能成长较快，制造业尤其是装备制造业增势较好。该省制造业增加值增长7.5%，其中装备制造业增长9.3%，明显快于规模以上工业增速。
+               Zhejiang's cohesive momentum conversion promotes the rapid growth of digital economy-related industries. In the first half of the year, the added value of the core industries of the digital economy increased by 14.8% year on year. The output value of new industrial products above the scale increased by 13.7% and that of new products was 35.7%.
             </p><br/>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;湖南的工业生产向中高端迈进。规模以上工业企业中，高加工度工业、高技术制造业增加值分别增长13.3%和14%。新能源汽车、光纤、工业机器人和锂离子电池等新产品产量分别增长2.6倍、1.7倍、81.4%和34%。从投资结构看，乡村振兴战略和创新引领战略加快推进。上半年，涉农投资、工业投资、产业投资分别增长39.2%、18%和21.6%；高新技术产业投资增长34.6%。</p><br/>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                The new momentum of Shanxi's industry is growing fast, and the growth of manufacturing industry, especially equipment manufacturing industry, is better. The added value of manufacturing industry in the province increased by 7.5%, of which the equipment manufacturing industry increased by 9.3%, significantly faster than the growth rate of industries above scale.
+            </p><br/>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hunan's industrial production has advanced to the middle and high end. Among Industrial Enterprises above the scale, the added value of high-processing industry and high-tech manufacturing industry increased by 13.3% and 14% respectively. The output of new energy vehicles, optical fibers, industrial robots and lithium-ion batteries increased by 2.6 times, 1.7 times, 81.4% and 34% respectively. From the perspective of investment structure, the strategy of Rural Revitalization and innovation-led strategy will be accelerated. In the first half of the year, agricultural investment, industrial investment and industrial investment increased by 39.2%, 18% and 21.6% respectively, and investment in high-tech industries increased by 34.6%.</p><br/>
 
-        　　<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;内蒙古在抓传统优势产业提质升级的同时特别注重培育和壮大新兴产业，带动全区经济新旧动能转换，优化产业结构，提升发展质量。上半年，一批具有战略性、创新性、前瞻性的工业新产品产量实现了较快增长。</p><br/>
+        　　<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Inner Mongolia pays special attention to cultivating and expanding new and emerging industries while improving the quality and upgrading of traditional superior industries, promoting the transformation of new and old economic momentum, optimizing industrial structure and improving the quality of development. In the first half of the year, the output of a batch of new industrial products with strategic, innovative and forward-looking characteristics increased rapidly.</p><br/>
 
-        　　<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;贵州增长动力“新”的因素在积累，新经济蓬勃发展。全省加快推进大数据与实体经济深度融合，产业发展的数字化、信息化、智能化改造提速。1月至5月，全省规模以上互联网和相关服务营业收入同比增长79.9%，软件和信息技术服务业营业收入增长27.4%。</p><br/>
+        　　<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The "new" factors of Guizhou's growth momentum are accumulating and the new economy is developing vigorously. The province has accelerated the deep integration of big data and real economy, and accelerated the digitalization, informatization and intelligent transformation of industrial development. From January to May, the operating income of Internet and related services above the province's scale increased by 79.9% year on year, and that of software and information technology services increased by 27.4%.</p><br/>
 
-        　　<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;随着创新驱动发展战略的深入实施，新产业、新业态、新商业模式的发展层出不穷，新技术、新产品、新服务不断涌现，新动能将成为保持经济平稳增长的重要动力。(记者林火灿)</p><br/>
+        　　<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;With the in-depth implementation of innovation-driven development strategy, new industries, new business models emerge in endlessly, new technologies, new products and new services continue to emerge, and new momentum will become an important driving force to maintain stable economic growth. (Reporter Lin Huochan)</p><br/>
         </div>
     </div>
 </template>
@@ -89,7 +89,7 @@ export default {
 .cn_bt {
     font-size: 4vh;
     /* height: 6vh; */
-    line-height: 7vh;
+    line-height: 5vh;
 }
 .cn_info {
     height: 80vh;
