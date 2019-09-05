cd95e47a8aff1801156336ee0ea20c245058407a
ningyufan <223_ningyufan@computer162.com>
2019-09-05 19:57:49 +0800

    nyf

--- pages/DigitalGX.vue
+++ pages/DigitalGX.vue
@@ -15,7 +15,7 @@
                                     <a href="#">
                                         <i class="el-icon-star-on"><label>中</label></i>
                                     </a>
-                                    <a href="/e_DigitalGX">
+                                    <a href="/e_digitalgx">
                                         <i class="el-icon-star-off"><label>英</label></i>
                                     </a>
                                 </div>
@@ -24,11 +24,11 @@
                                 <div class="nav">
                                     <ul>
                                         <nuxt-link to="/chinese_index"><li>首页</li></nuxt-link>
-                                        <nuxt-link to="/Digital_China"><li>数字中国</li></nuxt-link>
+                                        <nuxt-link to="/digital_china"><li>数字中国</li></nuxt-link>
                                         <nuxt-link to="#"><li><div class="nav_3">数字广西</div></li></nuxt-link>
-                                        <nuxt-link to="/DigitalYL"><li>数字玉林</li></nuxt-link>
-                                        <nuxt-link to="/DigitalYS"><li>数字玉师</li></nuxt-link>
-                                        <nuxt-link to="/aboutMe"><li>关于我们</li></nuxt-link>
+                                        <nuxt-link to="/digitalyl"><li>数字玉林</li></nuxt-link>
+                                        <nuxt-link to="/digitalys"><li>数字玉师</li></nuxt-link>
+                                        <nuxt-link to="/aboutme"><li>关于我们</li></nuxt-link>
                                     </ul>
                                 </div>
                             </el-row>
--- pages/e_DigitalGX.vue
+++ pages/e_DigitalGX.vue
@@ -12,7 +12,7 @@
                         <div class="header_nav">
                         <el-row class="row_one">
                             <div class="ce_nav">
-                                <a href="/DigitalGX">
+                                <a href="/digitalgx">
                                     <i class="el-icon-star-off"><label>Chinese</label></i>
                                 </a>
                                 <a href="#">
@@ -24,11 +24,11 @@
                             <div class="nav">
                                 <ul>
                                     <nuxt-link to="/english_index"><li>Home page</li></nuxt-link>
-                                    <nuxt-link to="/e_DigitalChina"><li>Digital China</li></nuxt-link>
+                                    <nuxt-link to="/e_digitalchina"><li>Digital China</li></nuxt-link>
                                     <nuxt-link to="#"><li><div class="nav_3">Digital Guangxi</div></li></nuxt-link>
-                                    <nuxt-link to="/e_DigitalYL"><li>Digital Yulin</li></nuxt-link>
-                                    <nuxt-link to="/e_DigitalYS"><li>Digital Yushi</li></nuxt-link>
-                                    <nuxt-link to="/e_aboutMe"><li>About us</li></nuxt-link>
+                                    <nuxt-link to="/e_digitalyl"><li>Digital Yulin</li></nuxt-link>
+                                    <nuxt-link to="/e_digitalys"><li>Digital Yushi</li></nuxt-link>
+                                    <nuxt-link to="/e_aboutme"><li>About us</li></nuxt-link>
                                 </ul>
                             </div>
                         </el-row>
